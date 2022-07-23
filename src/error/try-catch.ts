5978b59717c06ff875f7e5dddae581e6ec75557f
Jorge Tolentino <jorge.tolentino.g@gmail.com>
2022-07-23 17:55:48 -0500

    -

--- src/error/try-catch.ts
+++ src/error/try-catch.ts
@@ -1,25 +1,5 @@
 import { UnexpectedError } from './UnexpectedError'
 
-export function tryCatch (friendyMessage?: string) {
-  return function (target: any, propertyName: any, descriptor: any) {
-    const method = descriptor.value
-    const handleError = makeHandleError(target, propertyName, friendyMessage)
-
-    descriptor.value = function (...args: any) {
-      try {
-        const result = method.apply(this, args)
-        if (isPromise(result)) {
-          return result.catch(handleError)
-        }
-
-        return result
-      } catch (error) {
-        handleError(error)
-      }
-    }
-  }
-}
-
 function isPromise (object: any): object is Promise<any> {
   return !!object && typeof object.then === 'function'
 }
@@ -38,5 +18,25 @@ function makeHandleError (
       `${className}#${propertyName}: ${friendyMessage ?? reason}`,
       error
     )
+  }
+}
+
+export function tryCatch (friendyMessage?: string) {
+  return function (target: any, propertyName: any, descriptor: any) {
+    const method = descriptor.value
+    const handleError = makeHandleError(target, propertyName, friendyMessage)
+
+    descriptor.value = function (...args: any) {
+      try {
+        const result = method.apply(this, args)
+        if (isPromise(result)) {
+          return result.catch(handleError)
+        }
+
+        return result
+      } catch (error) {
+        handleError(error)
+      }
+    }
   }
 }
